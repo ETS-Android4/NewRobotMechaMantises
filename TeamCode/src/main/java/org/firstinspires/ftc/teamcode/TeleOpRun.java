6170f762a6de856791bf75ec5e139f136fccc5f7
AJain862 <72466106+AJain862@users.noreply.github.com>
2022-01-09 20:50:21 -0800

    New Robot Mecha Mantises

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpRun.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpRun.java
@@ -202,20 +202,23 @@ public class TeleOpRun extends OpMode {
             rotateintake.setPower(0);
 
         }
-        double y = -gamepad1.right_stick_y;
-        double x = gamepad1.right_stick_x ;
-        double rx = gamepad1.left_stick_x;
+        double y = -gamepad1.left_stick_y/4; // Remember, this is reversed!
+        double x = gamepad1.left_stick_x/4 * 1.1; // Counteract imperfect strafing
+        double rx = gamepad1.right_stick_x/4;
 
-        double denominator = Math.max(Math.abs(y)+Math.abs(x)+Math.abs(rx), 0.5);
-        double frontLeftPower = (y+x+rx)/denominator;
-        double backLeftPower = (y-x+rx)/denominator;
-        double frontRightPower = (y-x-rx)/denominator;
-        double backRightPower = (y+x-rx)/denominator;
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio, but only when
+        // at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
 
-        fl_wheel.setPower(frontLeftPower/2);
-        bl_wheel.setPower(backLeftPower/2);
-        fr_wheel.setPower(frontRightPower/2);
-        br_wheel.setPower(backRightPower/2);
+        fl_wheel.setPower(frontLeftPower);
+        bl_wheel.setPower(backLeftPower);
+        fr_wheel.setPower(frontRightPower);
+        br_wheel.setPower(backRightPower);
 
 
 
